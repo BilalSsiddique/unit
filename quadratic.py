160672654b5479b7101c86487c1f5ee81029e4ff
BilalSsiddique <bsiddiqui@students.uit.edu>
2022-04-18 21:26:47 +0500

    Added more conditions

--- quadratic.py
+++ quadratic.py
@@ -1,22 +1,36 @@
 import math
-
+import cmath
 
 def quadratic_func(a, b, c):
-    if type(a) is int and type(b) is int and type(c) is int:
-        disc = b * b - 4 * a * c
-        if a > 0:
-        
-            if disc == 0:
-                x1 = (-b + math.sqrt(disc)) / (2 * a)
-                return (x1,)
-            x1 = (-b + math.sqrt(disc)) / (2 * a)
-            x2 = (-b - math.sqrt(disc)) / (2 * a)
-            return x1, x2
+    try:
+        lst = [a, b, c]
+        if all(isinstance(n, int) for n in lst) or all(isinstance(n, float) for n in lst):
+            disc = b * b - 4 * a * c
+            
+            if a != 0:
+
+                if disc == 0:
+                    x1 = (-b + math.sqrt(disc)) / (2 * a)
+                    return (x1,)
+                elif disc>0:
+                    x1 = (-b + math.sqrt(disc)) / (2 * a)
+                    x2 = (-b - math.sqrt(disc)) / (2 * a)
+                    return x1, x2
+                else:
+                    x1 = (-b + cmath.sqrt(disc)) / (2 * a)
+                    x2 = (-b - cmath.sqrt(disc)) / (2 * a)
+                    return x1, x2
+
+            else:
+                return -1
         else:
             return -1
-    else:
-        return -1
+    except ValueError or TypeError:
+            return lst
+
+
+# print(quadratic_func(1,None, 1))
 
 
 
-# print(quadratic_func(2, 7, 5))
+
--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 from quadratic import quadratic_func
 import unittest
 
+
 class Tests(unittest.TestCase):
-    
-    def test_1(self):  #simple testcase 
-        result=(-2.0,-3.0)
-        self.assertEqual(result,quadratic_func(1,5,6))
+
+    def test_1(self):  # simple testcase
+        result = (-2.0, -3.0)
+        self.assertEqual(result, quadratic_func(1, 5, 6))
+
     def test_2(self):
-        result= (-1,) # test_case for one solution ex: x1 will only return
-        self.assertEqual(result,quadratic_func(1,2,1))
+        result = (-1,)  # test_case for one solution ex: x1 will only return
+        self.assertEqual(result, quadratic_func(1, 2, 1))
+
     def test_3(self):
-        result= -1        #test_case for checking string values         
+        result = -1  # test_case for checking string values
         self.assertEqual(result, quadratic_func(1, "2", 1))
 
-
+    def test_4(self):
+        result = -1  # test_case for checking None
+        self.assertEqual(result, quadratic_func(1, None, 1))
 
 
 if __name__ == '__main__':
